e0e9e7373ab198d57b1a6b014771a91570edeef7
Photon <photon78@users.noreply.github.com>
2026-04-06 11:05:02 +0200

    chore: remove debug test scripts

--- scripts/test_import.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.insert(0, "src")
-import main
-print("Import OK")
-print("Commands:", [c.name for c in main.app.registered_commands])
--- scripts/test_venv.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-print("Python:", sys.executable)
-print("Path:", sys.path[:3])
-try:
-    import questionary
-    print("questionary OK:", questionary.__version__)
-except ImportError as e:
-    print("questionary FAIL:", e)
